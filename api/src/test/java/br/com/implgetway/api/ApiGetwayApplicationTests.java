4f1d35783e6a9109245445d928bcc24c22b900df
IAbrahanArley <abrahanarley96@gmail.com>
2024-02-14 08:31:20 -0300

    projeto inicial

---
+++ api/src/test/java/br/com/implgetway/api/ApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.implgetway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
