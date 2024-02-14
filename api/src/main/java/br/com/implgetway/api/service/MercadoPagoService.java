120730628d2da37777f5fd803b520f3e8d90c67e
IAbrahanArley <abrahanarley96@gmail.com>
2024-02-14 10:04:37 -0300

    Feat: Criando item e pagador

--- api/src/main/java/br/com/implgetway/api/service/MercadoPagoService.java
+++ api/src/main/java/br/com/implgetway/api/service/MercadoPagoService.java
@@ -2,16 +2,19 @@ package br.com.implgetway.api.service;
 
 import com.mercadopago.MercadoPagoConfig;
 import com.mercadopago.client.MercadoPagoClient;
-import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
-import com.mercadopago.client.preference.PreferenceClient;
-import com.mercadopago.client.preference.PreferencePaymentMethodsRequest;
-import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.client.common.AddressRequest;
+import com.mercadopago.client.common.IdentificationRequest;
+import com.mercadopago.client.common.PhoneRequest;
+import com.mercadopago.client.preference.*;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.preference.Preference;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MercadoPagoService {
@@ -20,8 +23,8 @@ public class MercadoPagoService {
 
         MercadoPagoConfig.setAccessToken("TEST-6588139419036949-020912-42e70f317b1ccb35ccf70b43bcbeefb9-1639440789");
         PreferenceRequest preferenceRequest = PreferenceRequest.builder()
-                .items(createItems)
-                .payer(createPayer)
+                .items(createItems())
+                .payer(createPayer())
                 .backUrls(PreferenceBackUrlsRequest.builder()
                         .success("https://localhost:8080/checkout/sucess")
                         .pending("https://localhost:8080/checkout/pending")
@@ -45,5 +48,47 @@ public class MercadoPagoService {
         // Printa o url do pagamento
         System.out.println(resultset.getInitPoint());
         return resultset.getId();
+    }
+
+    private List<PreferenceItemRequest> createItems(){
+        List<PreferenceItemRequest> items = new ArrayList<>();
+
+        PreferenceItemRequest preferenceItemRequest = PreferenceItemRequest.builder()
+                .id("Item-D143F")
+                .title("Macbook Air")
+                .description("Notebook Macbook Air Apple, Tela de Retina 13, M2, 8GB RAM, CPU 8 Núcleos, GPU 8 Núcleos, SSD 256GB, Cinza Espacial - MLXW3BZ/A")
+                .categoryId("Notebook")
+                .quantity(1)
+                .unitPrice(BigDecimal.valueOf(9000.00))
+                .currencyId("BRL")
+                .pictureUrl("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=452&hei=420&fmt=jpeg&qlt=95&.v=1664472289661")
+                .build();
+        items.add(preferenceItemRequest);
+        return items;
+
+    }
+
+    private PreferencePayerRequest createPayer(){
+
+        PreferencePayerRequest payer = PreferencePayerRequest.builder()
+                .name("Fulano")
+                .surname("Pagador Test")
+                .email("Fulano@pagador.com")
+                .phone(PhoneRequest.builder()
+                        .areaCode("83")
+                        .number("991754322")
+                        .build())
+                .identification(IdentificationRequest.builder()
+                        .type("CPF")
+                        .number("12345678909")
+                        .build())
+                .address(AddressRequest.builder()
+                        .streetName("")
+                        .streetName("")
+                        .zipCode("")
+                        .build())
+                .build();
+
+        return payer;
     }
 }
